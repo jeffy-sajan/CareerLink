7c659ec841dc49bae360e996c500d8539917e06b
jeffy-sajan <jeffysajan9400@gmail.com>
2025-05-20 20:09:13 +0530

    commmmit

--- carrerlink/settings.py
+++ carrerlink/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,10 +55,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'carrerlink.urls'
-
-
-import pymysql
-pymysql.install_as_MySQLdb()
 
 
 TEMPLATES = [
